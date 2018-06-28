9455299615fba14c8fd16b769727e4a0176df76a
TakashiKusachi <b514410@ns.kogakuin.ac.jp>
2018-06-28 04:55:56 +0000

    is running

--- src/motor_system/src/main.cpp
+++ src/motor_system/src/main.cpp
@@ -5,52 +5,85 @@
 #include<string>
 
 class MotorSystem{
+
+/*	MotorSystem menver	*/
+private:
+	unsigned char id;
+	float velocity;
+
+	enum{
+		INIT,
+		BEGIN_WAIT,
+		WAIT,
+		RUNNING,
+	}state;
+
+/*	ROS menber		*/
+private:
 	ros::NodeHandle nh;
 	ros::Subscriber vel_topic;
-	ros::Publisher can_bus;
+	ros::Publisher sent_can_bus;
+	ros::Subscriber receive_can_bus;
+	
+	ros::Timer timer;
+private:
+	bool is_my_message(can_msgs::Frame msg){
+		return (msg.id & 0x0f) == id;
+	}
+	//void _send_velocity(float vel);
+	//void _send_begin();
+
 public:
-	MotorSystem();
+	MotorSystem(unsigned char);
 	void SetVelocity(const motor_system::velocity);
+	void CatchMsg(const can_msgs::Frame);
+
+	void Process(const ros::TimerEvent&);
 };
 
-MotorSystem::MotorSystem()
+MotorSystem::MotorSystem(unsigned char _id=0)
 {
+	state = INIT;
+	this->id = _id;
+	this->velocity = 0;
 	vel_topic = nh.subscribe("SetVelocity",1000,&MotorSystem::SetVelocity,this);
-	can_bus = nh.advertise<can_msgs::Frame>("/can0/sent",1000);
-	ROS_INFO_STREAM("MotorSystem Constract");
+	sent_can_bus = nh.advertise<can_msgs::Frame>("/can0/sent",1000);
+	receive_can_bus = nh.subscribe("/can0/receive",1000,&MotorSystem::CatchMsg,this);
+	this->timer = nh.createTimer(ros::Duration(0.01),&MotorSystem::Process,this);
+	ROS_INFO_STREAM("MotorSystem Constracter");
+	ROS_INFO_STREAM("Send Begin Message");
 }
 
 void MotorSystem::SetVelocity(const motor_system::velocity vel)
 {
-	std::stringstream ss;
+	this->velocity = vel.velocity;
+}
+
+void MotorSystem::CatchMsg(const can_msgs::Frame msg)
+{
+	if(msg.id & 0x0f)return ;
+}
+
+void MotorSystem::Process(const ros::TimerEvent& e)
+{
+	//ROS_INFO_STREAM("TIMER");
 	can_msgs::Frame msg;
-	msg.id=0x00;
-	msg.is_rtr=false;
-	msg.is_extended = false;
+	msg.id = this->id;
+	msg.is_rtr = false;
+	msg.is_extended=false;
 	msg.dlc=4;
-	union{
-		struct{
-			float f;
-			float dumy;
-		};
-		struct{
-			unsigned char c[8];
-		};
-	}data;
-	data.f = vel.velocity;
-	msg.data[0] = data.c[0];
-	msg.data[1] = data.c[1];
-	msg.data[2] = data.c[2];
-	msg.data[3] = data.c[3];
-	ss<<"velocity "<<vel.velocity;
-	can_bus.publish(msg);
-	ROS_INFO_STREAM(ss.str());
+	msg.data[0] = 0;
+	msg.data[1] = 0;
+	msg.data[2] = 0;
+	msg.data[3] = 0;
+	sent_can_bus.publish(msg);
+	
 }
 
 int main(int argc,char **argv)
 {
 	ros::init(argc,argv,"motor_system");
-	MotorSystem motor;
+	MotorSystem motor(0x08);
 	ros::spin();
 	return 0;
 }
